34be0122c9efd31afce7053eede60934e2ac81ea
oxe410 <oxe410@cs.bham.ac.uk>
2016-11-25 15:39:27 +0000

    Added kernel write

--- Setup/firewallSetup.c
+++ Setup/firewallSetup.c
@@ -1,6 +1,23 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int check_rule(char *line)
+{
+	int port;
+	char filename[200];
+
+	if (sscanf(line, "%d %s", &port, filename) <= 0)
+		return 1;
+
+	// Check executable in ret_filename
+
+	return 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -15,14 +32,83 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	else {
+		char *proc_filename, *cmd;
+		int fd;
+
+		proc_filename = "/proc/firewallExtension";
+		if ((fd = open(proc_filename, O_WRONLY)) == -1) {
+			printf("Error: cannot open proc file: %d\n", errno);
+			return 1;
+		}
+		cmd = argv[1];
+
 		if(strcmp(argv[1],"L")==0){
-			printf("Print current policy\n");
-			// TODO: Complete me
+			//printf("Print current policy\n");
+			if (write(fd, "L", 1) != 1) {
+				printf("Error in writing to proc file.\n");
+				return 1;
+			}
 			return 0;
 		} else {
 			if(strcmp(argv[1],"W")==0){
-				printf("Reading commands\n");
-				// TODO: Complete me
+				//printf("Reading commands\n");
+				FILE *fp = fopen(argv[2], "r");
+				if (fp == NULL) {
+					printf("Error in opening rules file.\n");
+					return 1;
+				}
+
+				char *line = NULL, *to_send = malloc(sizeof(char) * 2);
+				int res, read, idx = 2, len = 0;
+				to_send[0] = 'W';
+				to_send[1] = ':';
+
+				while ((read = getline(&line, &len, fp)) != -1) {
+					res = check_rule(line);
+					if (res == 1) {
+						printf("ERROR: Ill-formed file\n");
+						return res;
+					} else if (res == 2) {
+						printf("ERROR: Cannot execute file\n");
+						return res;
+					}
+
+					int line_len = strlen(line);
+					to_send = realloc(to_send, sizeof(char) * (idx + line_len));
+					strncpy(to_send + idx, line, line_len); // line_len -> does not include null terminator
+					idx += line_len;
+
+					/*free(line);
+					line = NULL;
+					len = 0;*/
+
+					//to_send = realloc(to_send, sizeof(char) * (i + 1));
+					//to_send[]
+
+					// Send string to firewall extension
+					//int str_len = strlen(line) + 2;
+					//char to_send[str_len];
+					//sprintf(to_send, "%s:%s", cmd, line);
+					/*if (write(fd, line, read) != read) {
+						printf("Error in writing to proc file\n");
+						return 1;
+					}*/
+				}
+
+				to_send[idx] = '\0';
+				len = strlen(to_send);
+
+				if (write(fd, to_send, len) != len) {
+					printf("Error in writing to proc file: %d\n", errno);
+					return 1;
+				}
+
+				fclose(fp);
+				if (to_send)
+					free(to_send);
+				if (line)
+					free(line);
+
 				return 0;
 			} else{
 				printf("Usage:\n ");
--- firewallExtension.c
+++ firewallExtension.c
@@ -1,21 +1,192 @@
-#include <linux/module.h>  /* Needed by all modules */
-MODULE_AUTHOR ("1230806");
-MODULE_DESCRIPTION ("Extensions to the firewall") ;
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/list.h>
+#include <linux/netfilter.h> 
+#include <linux/netfilter_ipv4.h>
+#include <net/tcp.h>
+
+// QUESTIONS
+// - Should I open the file or pass the string from user space
+
+MODULE_AUTHOR ("1466610");
+MODULE_DESCRIPTION ("Extensions to the firewall");
 MODULE_LICENSE("GPL");
 
+// Data structures
+// -------------------------------------------------------------------
+struct f_rule {
+	int port;
+	char *program;
+	struct list_head list;
+};
 
+// Globals
+// -------------------------------------------------------------------
+#define PROC_ENTRY_FILENAME "firewallExtension"
+#define LIST_RULES 'L'
+#define ADD_RULES 'W'
 
-int init_module(void)
+static int proc_open = 0;
+static struct proc_dir_entry *proc_file;
+//static struct list_head f_rule_list;
+static struct f_rule f_rule_list;
+//INIT_LIST_HEAD(&f_rule_list);
+
+DEFINE_MUTEX(proc_lock);
+
+unsigned int firewall_ext_hook (const struct nf_hook_ops *ops,
+				    struct sk_buff *skb,
+				    const struct net_device *in,
+				    const struct net_device *out,
+				    int (*okfn)(struct sk_buff *)) {
+
+	return NF_ACCEPT;
+}
+
+// W:rules_string
+ssize_t k_write(struct file *file, const char __user *buffer, size_t count, loff_t *offset)
 {
-	printk(KERN_INFO "Initializing kernel module:\n");
+	// Parse input (error handling is done in the user space)
+	int len = count + 1;
+	char *buf = kmalloc(sizeof(char) * len, GFP_KERNEL), cmd;
+	
+	
+	strncpy_from_user(buf, buffer, len - 1);
+	buf[len - 1] = '\0';
 
-  	// A non 0 return means init_module failed; module can't be loaded.
+	printk("Buffer: %s\n", buf);
+
+	// Extract command
+	cmd = buf[0];
+	printk("This is cmd: %c\n", cmd);
+
+	if (cmd == LIST_RULES) {
+		struct f_rule *rule;
+		list_for_each_entry(rule, &f_rule_list.list, list) {
+			printk("Firewall rule: %d %s\n", rule->port, rule->program);
+		}
+	} else if (cmd == ADD_RULES) {
+		char *rules = buf + 2, port[10], *program = NULL;
+		int rules_len = strlen(rules), i, c = 0, parsing_phase = 1, dec_port;
+
+		// Remove previous rules
+		printk("HERE WE ARE!");
+
+		// Add new rules
+		for (i = 0; i < rules_len; i++) {
+			if (rules[i] == '\n') {
+				// Save program and rule
+				program[c] = '\0';
+
+				struct f_rule *new_rule = kmalloc(sizeof(struct f_rule), GFP_KERNEL);
+				new_rule->port = dec_port;
+				new_rule->program = kmalloc(sizeof(char) * (strlen(program) + 1), GFP_KERNEL);
+				strcpy(new_rule->program, program);
+				INIT_LIST_HEAD(&new_rule->list);
+				list_add(&new_rule->list, &f_rule_list.list);
+
+				kfree(program);
+				program = NULL;
+				c = 0;
+				parsing_phase = 1;
+				continue;
+			} else if (rules[i] == ' ' && parsing_phase == 1) {
+				// Save port
+				port[c] = '\0';
+				kstrtoint(port, 0, &dec_port);
+				c = 0;
+				parsing_phase = 2;
+				continue;
+			}
+
+			if (parsing_phase == 1) {
+				port[c++] = rules[i];
+			} else {
+				program = krealloc(program, sizeof(char) * (c + 1), GFP_KERNEL);
+				program[c++] = rules[i];
+			}
+		}
+
+		// Check rules
+
+	}
+
+	// Free buf
+	return count;
+}
+
+int procfs_open(struct inode *inode, struct file *file)
+{
+	mutex_lock(&proc_lock);
+	if (proc_open) {
+		mutex_unlock(&proc_lock);
+		return -EAGAIN;
+	}
+	proc_open++;
+	printk (KERN_INFO "proc file opened\n");
+	mutex_unlock(&proc_lock);
+	try_module_get(THIS_MODULE);
+
 	return 0;
 }
 
+int procfs_close(struct inode *inode, struct file *file)
+{
+	mutex_lock(&proc_lock);
+	proc_open--;
+	printk (KERN_INFO "proc file closed\n");
+	mutex_unlock(&proc_lock);
+
+	module_put(THIS_MODULE);
+	return 0;
+}
+
+static struct file_operations fops = {
+	.owner 		= THIS_MODULE,
+	.write 		= k_write,
+	.open 		= procfs_open,
+	.release 	= procfs_close
+};
+
+/*static struct nf_hook_ops firewall_ext_ops = {
+	.hook 		= firewall_ext_hook,
+	.pf 		= PF_INET,
+	.priority 	= NF_IP_PRI_FIRST,
+	.hooknum 	= NF_INET_LOCAL_OUT
+};*/
+
+int init_module(void)
+{
+	printk(KERN_INFO "Initializing firewallExtension module:\n");
+
+	// Initialise list
+	INIT_LIST_HEAD(&f_rule_list.list);
+
+	// Create proc file
+	proc_file = proc_create_data(PROC_ENTRY_FILENAME, 0666, NULL, &fops, NULL); // 0644
+	if (proc_file == NULL) {
+		printk(KERN_ALERT "Error: Could not initialize /proc/%s\n", PROC_ENTRY_FILENAME);
+		return -ENOMEM;
+	}
+
+	printk(KERN_INFO "/proc/%s created\n", PROC_ENTRY_FILENAME);
+
+	// Register firewall hook
+	/*int errno = nf_register_hook(&firewall_ext_ops);
+	if (errno) {
+		printk(KERN_ALERT "Firewall extension could not be registered!\n");
+		return errno;
+	}
+
+	printk(KERN_INFO "Firewall extension module loaded\n");*/
+
+	return 0;
+}
 
 void cleanup_module(void)
 {
+	remove_proc_entry(PROC_ENTRY_FILENAME, NULL);
 	printk(KERN_INFO "Firewall extensions module unloaded\n");
-}  
-
+}
